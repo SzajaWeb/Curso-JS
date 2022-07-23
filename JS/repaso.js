3b2ae5649112c657843b9e3fad74c3fbae4b6f9d
SzajaWeb <szajajose@gmail.com>
2022-07-23 12:08:41 -0300

    first commit

---
+++ JS/repaso.js
@@ -0,0 +1,49 @@
+ /* objeto console 
+
+        .log ("aqui va el mensaje")
+        .warn ("aqui va el mensaje amarillo") signo de advertencia
+        .error ("aqui va el mensaje rojo") signo de error
+
+ 
+ */
+/*
+cuadros de dialogo
+
+    alert()
+    confirm() confirmar una accion  aceptar o cancelar
+    prompt() ingresar un dato en pantalla
+*/          
+
+
+
+    /*
+    let miVariable
+    let miVariable2 = "algo"
+    let edad = 27
+    let booleanas true false
+    
+    */
+
+/*
+    let username = "Coderhouse"
+    let edad = 21
+
+
+    if( username == "Coderhouse"){
+        console.log("bienvenido", username)
+    }*/
+
+//parseInt()  parseFloat()
+
+ 
+ let username = prompt("ingrese su nombre:")
+
+debugger
+
+
+    if( username == "Coderhouse"){
+        console.log("bienvenido", username)
+    } else{
+        console.warn("no se reconoce el usuario")
+    }
+    
