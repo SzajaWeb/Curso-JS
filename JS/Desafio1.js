1857f7da634c6140ac8057d9123831d09cf12308
SzajaWeb <szajajose@gmail.com>
2022-07-28 12:12:35 -0300

    subida desafio 2

--- JS/Desafio1.js
+++ JS/Desafio1.js
@@ -1,7 +1,30 @@
 debugger
-let variable = parseInt(prompt("ingrese un numero:"))
+let cuotas = 0
+let valorf = 0
+let valort = 0
 
-for( i=0; i<variable; i++){
-    console.log("hola", i)
-    
+function calcInteres (){
+    valort = valori * 1.05
+    valorf = valort / cuotas
+    confirm(`el valor de su cuota es de $ ${valorf} y el valor total es $ ${valort}`)
+}
+
+let valori = parseFloat(prompt("ingrese el valor de su compra a cuotificar: "))
+
+do{
+cuotas = parseInt(prompt("ingrese la cantidad de cuotas: 1 3 6 12, tenga en cuenta que en 1 y 3 pagos no tiene recargo, en 6 y 12 tiene recargo del 5%"))
+} while(cuotas != 1 && cuotas != 3 && cuotas!= 6 && cuotas != 12)
+
+if(cuotas == 1){
+    confirm("el valor de su cuota es de $" + valori)
+
+}    else if (cuotas == 3){
+    valorf = valori / cuotas
+    confirm("el valor de su cuota es de $" + valorf) 
+
+}    else if(cuotas == 6){
+    calcInteres();
+
+}   else if(cuotas == 12){
+    calcInteres();
 }
