d32d6b9189ff267539aca7ae7d4fa8da8d9eadb8
SzajaWeb <szajajose@gmail.com>
2022-08-25 12:20:32 -0300

    subida

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,6 @@
 const carrito = [];
 const cuerpo = document.getElementById("carritoProds");
 
-  
 function recuperoProdsLS() {
   if (localStorage.carrito) {
     const prodsGuardados = JSON.parse(localStorage.getItem("carrito"));
@@ -13,9 +12,9 @@ function recuperoProdsLS() {
 }
 let precioFinal = 0;
 function cargarProds() {
-  if( carrito != ""){
-  cuerpo.innerHTML = "";
-}
+  if (carrito != "") {
+    cuerpo.innerHTML = "";
+  }
   carrito.forEach((prodAgr) => {
     cuerpo.innerHTML += `
       <ul class = "listaProdsCarro">
@@ -26,11 +25,9 @@ function cargarProds() {
       <li>Cant
       : ${prodAgr.cantidad}</li>
       </ul>`;
-    precioFinal = precioFinal + prodAgr.precio*prodAgr.cantidad;
+    precioFinal = precioFinal + prodAgr.precio * prodAgr.cantidad;
   });
 }
-
-
 
 document.getElementById("btnVaciar").addEventListener("click", () => {
   localStorage.removeItem("carrito");
@@ -38,13 +35,45 @@ document.getElementById("btnVaciar").addEventListener("click", () => {
 });
 
 document.getElementById("btnComprar").addEventListener("click", () => {
-    const finCompra = document.getElementById("carritoComp");
-    finCompra.innerHTML = `<div id="finCompra">
-    <p id= "txtFinCompra" >GRACIAS POR SU COMPRA</p>
-  </div>`
-})
+  Swal.fire({
 
-const txtPrecio = document.querySelector("#precioFinal");
-txtPrecio.innerText = `PRECIO: $ ${precioFinal}`;
+    title: "Desea cuotificar su compra?",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "SI",
+    denyButtonText: `NO`,
+  }).then((result) => {
+  
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "selecciona la cantidad de cuotas",
+        input: "select",
+        inputOptions: {
+          Cuotas: {
+            unacuota: "1 cuota",
+            trescuotas: "3 cuotas",
+            seiscuotas: "6 cuotas",
+            doceCuotas: "12 cuotas",
+          },
+        },
+      });
+
+      if (Cuotas == unacuota && Cuotas == trescuotas) {
+        Swal.fire({
+          icon: "success",
+        });
+      }
+    } else if (result.isDenied) {
+      Swal.fire({
+        icon: "success",
+        title: "Gracias por su compra!!",
+      });
+    }
+  });
+});
+
+
 recuperoProdsLS();
 cargarProds();
+const txtPrecio = document.querySelector("#precioFinal");
+txtPrecio.innerText += `PRECIO: $ ${precioFinal}`;
