e6eee17b5b84af020faa95401135d714220889ea
SzajaWeb <szajajose@gmail.com>
2022-09-13 09:12:40 -0300

    subida

--- js/index.js
+++ js/index.js
@@ -1,64 +1,67 @@
 (async function () {
-  const respuesta = await fetch('../productos.json');
+  const respuesta = await fetch("../productos.json");
   const origenes = await respuesta.json();
 
+  const carrito = [];
 
+  if (localStorage.carrito) {
+    const recuperoCarrito = JSON.parse(localStorage.getItem("carrito"));
+    recuperoCarrito.forEach((prodCarrito) => {
+      carrito.push(prodCarrito);
+    });
+  }
+  const producto = document.getElementById("productos");
 
-const carrito = [];
+  function crearProducto() {
+    for (let prod of origenes) {
+      let contenedor = document.createElement("div");
+      contenedor.className = "cafe";
 
-const producto = document.getElementById("productos");
+      const image = document.createElement("img");
+      image.className = "imgBolsa";
+      image.src = "../img/bolsa cafe.jpg";
+      contenedor.appendChild(image);
 
-function crearProducto() {
-  for (let prod of origenes) {
-    let contenedor = document.createElement("div");
-    contenedor.className = "cafe";
-
-    const image = document.createElement("img");
-    image.className = "imgBolsa";
-    image.src = "../img/bolsa cafe.jpg";
-    contenedor.appendChild(image);
-
-    let lista = document.createElement("ul");
-    lista.id = "txtProd";
-    lista.innerHTML = `<li>Origen: ${prod.origen}</li>
+      let lista = document.createElement("ul");
+      lista.id = "txtProd";
+      lista.innerHTML = `<li>Origen: ${prod.origen}</li>
     <li>Precio: ${prod.precio}</li>
     <li>Peso: ${prod.peso}</li>`;
-    contenedor.appendChild(lista);
+      contenedor.appendChild(lista);
 
-    let btn = document.createElement("button");
-    btn.innerText = "Agregar al carrito";
-    btn.className = "btnAgr";
-    btn.id = `prodid${prod.id}`;
+      let btn = document.createElement("button");
+      btn.innerText = "Agregar al carrito";
+      btn.className = "btnAgr";
+      btn.id = `prodid${prod.id}`;
 
-    contenedor.appendChild(btn);
+      contenedor.appendChild(btn);
 
-    producto.appendChild(contenedor);
-  }
-  agregarClick();
-}
-
-crearProducto();
-
-function agregarClick() {
-  origenes.forEach((prods) => {
-    document
-      .querySelector(`#prodid${prods.id}`)
-      .addEventListener("click", () => {
-        agregarAlCarrito(prods);
-      });
-  });
-}
-
-function agregarAlCarrito(prods) {
-  let existe = carrito.some((productoSome) => productoSome.id === prods.id);
-  if (existe === false) {
-    prods.cantidad = 1;
-    carrito.push(prods);
-  } else {
-    prods.cantidad++;
+      producto.appendChild(contenedor);
+    }
+    agregarClick();
   }
 
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-}
+  crearProducto();
 
+  function agregarClick() {
+    origenes.forEach((prods) => {
+      document
+        .querySelector(`#prodid${prods.id}`)
+        .addEventListener("click", () => {
+          agregarAlCarrito(prods);
+        });
+    });
+  }
+
+  function agregarAlCarrito(prods) {
+    let existe = carrito.some((productoSome) => productoSome.id === prods.id);
+    if (existe === false) {
+      prods.cantidad = 1;
+      carrito.push(prods);
+    } else {
+      prods.cantidad++;
+    }
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }
 })();
